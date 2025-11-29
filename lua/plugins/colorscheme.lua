f9e7a71fca79d345116cdf0f1c381c6f6ca20d76
antoj2 <antonfurbojensen@gmail.com>
2025-11-29 23:39:15 +0100

    add colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,9 @@
-vim.pack.add { 'https://github.com/vague-theme/vague.nvim.git' }
-require('vague').setup {}
-vim.cmd.colorscheme 'vague'
+-- vim.pack.add { 'https://github.com/vague-theme/vague.nvim.git' }
+-- require('vague').setup {}
+-- vim.cmd.colorscheme 'vague'
 
 -- vim.pack.add { { src = 'https://github.com/Kaikacy/Lemons.nvim.git', version = vim.version.range '*' } }
 -- vim.cmd.colorscheme 'lemons'
+
+vim.pack.add { 'https://github.com/nendix/zen.nvim.git' }
+vim.cmd.colorscheme 'zen'
